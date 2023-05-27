e24509b40bbd0ef9e59c2398ab5e6c60d543be20
YmniyKakYtka <jmaster21042005@mail.ru>
2023-05-27 09:59:53 +0300

    params video system

--- src/components/Pages/Video/Video.tsx
+++ src/components/Pages/Video/Video.tsx
@@ -2,15 +2,19 @@ import * as React from 'react';
 import Player from '../../UI/Player/Player';
 import styles from './Video.module.css';
 
-import video from '../../../assets/videos/mihailo.mp4';
+import { useTSelector } from '../../../hooks/redux';
+import { useParams } from 'react-router-dom';
 
 interface IVideoProps {
 }
 
 const Video: React.FC<IVideoProps> = (props) => {
+  const { id } = useParams();
+  const video = useTSelector(state => state.videos.list).find(video => video.id === id);
+
   return (
     <div className={styles.body}>
-      <Player video={video} autoPlay={true} />
+      <Player video={video?.video} autoPlay={true} />
     </div>
   );
 };
--- src/components/UI/Player/Player.tsx
+++ src/components/UI/Player/Player.tsx
@@ -2,16 +2,16 @@ import * as React from 'react';
 import styles from './Player.module.css';
 
 interface IPlayerProps {
-    video: string;
+    video?: string;
     autoPlay: boolean;
 }
 
 const Player: React.FC<IPlayerProps> = ({ video, autoPlay, ...props }) => {
-  return (
-    <div className={styles.body}>
-        <video src={video} controls autoPlay={autoPlay} />
-    </div>
-  );
+    return (
+        <div className={styles.body}>
+            <video src={video} controls autoPlay={autoPlay} />
+        </div>
+    );
 };
 
 export default Player;
